365330c8e6611716c3b19eb3c82bd31ed27cb058
Sahil jain <87411908+2sahil3@users.noreply.github.com>
2022-10-30 15:04:02 +0530

    Add files via upload
    Added comments for easier understanding of code

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+# Software starts with main.py
+
+
 from interface import unauthorized
 from model import speaker_identifier
 
 if __name__ == "__main__":
+
+    #predict the user using speaker_identifier method from model
     pred = speaker_identifier()
+
     if pred == 1:
         print("You are authorized!")
+        #if authorized start interacting and launch voice assistant
         exec(open("D:\\Sahil\\coding\\Python\\voice biometrics\\interface.py").read())
     else:
         print("Unauthorized !!")
+        #speak "You are unauthorized"
         unauthorized()
 
 

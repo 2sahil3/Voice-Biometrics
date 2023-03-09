b89dc099c2b5e23e242bae56f8a63eb72b96d9cd
Sahil jain <87411908+2sahil3@users.noreply.github.com>
2023-03-09 18:28:01 +0530

    Add files via upload
    created methods for MFCCs

--- creatingData.py
+++ creatingData.py
@@ -33,7 +33,7 @@ def record_audio():
         if choice == 0:
             break
 
-#used in model.py
+#used in model.py / used to return 40 mfccs of a particular audio file.
 def extract_mfcc(file, n_mfcc=40):
     audio, sr = librosa.load(file)
 
@@ -44,31 +44,29 @@ def extract_mfcc(file, n_mfcc=40):
 
 
 # Extract mfccs
-#
-# df = pd.DataFrame(columns=range(0,40))
-# print(df.columns)
-# for i in range(0, 21):
-#     fileName = "data\\prakhar" + str(i) + ".wav"
-#
-#     mfccs = extract_mfcc(fileName, 40)
-#     lst = list(mfccs)
-#     df.loc[len(df)] = lst
-#
-# print(df)
-# df.to_csv("data\sahil\\prakhar.csv") #dont forget to change the paths (there are literally many paths in the project)
-
+def createMfccCsv():
+    df = pd.DataFrame(columns=range(0,40))
+    for i in range(0, 21):
+        fileName = "data\\prakhar" + str(i) + ".wav"
+        mfccs = extract_mfcc(fileName, 40)
+        lst = list(mfccs)
+        df.loc[len(df)] = lst
+    df.to_csv("data\sahil\\prakhar.csv") #dont forget to change the paths (there are literally many paths in the project)
 
 
 # Append all individual data to complete data.
-#
-# df1 = pd.read_csv("data\sahil\\complete_data.csv")
-# print(df1.shape)
-# df2 = pd.read_csv("data\sahil\\prakhar.csv")
-# print(df2.shape)
-# df1 = df1.append(df2, ignore_index=True)
-# print(len(df1))
-# df1.to_csv("data\sahil\complete_data.csv")
+def appendIndividualToCompleteCSV():
+    df1 = pd.read_csv("data\sahil\\complete_data.csv") #read completedata.csv
+    df2 = pd.read_csv("data\sahil\\prakhar.csv") #read speaker_name.csv
+    df1 = df1.append(df2, ignore_index=True) #append df2 into df1
+    df1.to_csv("data\sahil\complete_data.csv") # save df1 in complete_data.csv (!! Note that the csv file should not be open in excel, that will give error).
 
 
-
+#call this function to record audio files
 # record_audio()
+
+# call this function to create speaker_name.csv files of individual speakers
+# createMfccCsv()
+
+#call this function to append speaker_name.csv of individual speaker into completedata.csv
+# appendIndividualToCompleteCSV()
